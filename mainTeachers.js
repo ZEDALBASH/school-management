5957f3aa65dfa6e2c6ebff8c7074b20c76d7c890
zedalbash <zedalbash342@gmail.com>
2024-03-21 23:54:54 +0300

    teachers js page done

---
+++ mainTeachers.js
@@ -0,0 +1,174 @@
+var Name = document.getElementById("nameInputTeachers");
+var birthdate = document.getElementById("birthdateInputTeachers");
+var address = document.getElementById("addressInputTeachers");
+var submit = document.getElementById("submitTeachers");
+var searchNameBtn = document.getElementById("searchNameBtnTeachers");
+var searchInput = document.getElementById("searchInputTeachers");
+var searchNumBtn = document.getElementById("searchNumBtnTeachers");
+
+var num = (function () {
+  const results = localStorage.getItem("results");
+
+  try {
+    let parseResults = JSON.parse(results);
+
+    if ((!Array.isArray(parseResults), parseResults != null)) {
+      throw new Error("is not array");
+    }
+    return Math.max(...parseResults.map((item) => item.Num));
+  } catch (err) {
+    console.log(err);
+    return 0;
+  }
+})();
+var mode = "create";
+var arryIndex;
+var dataArry = [];
+if (localStorage.results != null) {
+  dataArry = JSON.parse(localStorage.results);
+} else {
+  dataArry = [];
+}
+
+submit.onclick = function createEdit(i) {
+  if ((Name.value != "", birthdate.value != "", address.value != "")) {
+    let result = {
+      Num: num,
+      name: Name.value,
+      birthdate: birthdate.value,
+      address: address.value,
+    };
+
+    if (mode == "create") {
+      result.Num += 1;
+      num++;
+      console.log(result);
+      dataArry.push(result);
+    } else if (mode == "edit") {
+      dataArry[arryIndex] = result;
+      mode = "create";
+      submit.innerHTML = "Add";
+    }
+    localStorage.setItem("results", JSON.stringify(dataArry));
+    console.log(result);
+    clearInput();
+  } else {
+    alert("field must not be empty!");
+  }
+
+  readData();
+};
+function clearInput() {
+  Name.value = "";
+  address.value = "";
+  birthdate.value = "";
+}
+
+function readData() {
+  let table = "";
+  for (let i = 0; i < dataArry.length; i++) {
+    table += `
+        <tr>
+                <td class="td-Num">${dataArry[i].Num}</td>
+                <td class="td-border"></td>
+                <td class="td-Name">${dataArry[i].name}</td>
+                <td class="td-border"></td>
+                <td class="td-Birthdate">${dataArry[i].birthdate}</td>
+                <td class="td-border"></td>
+                <td class="td-Address">${dataArry[i].address}</td>
+                <td class="td-border"></td>
+                <td class="td-Delete-and-Edit"><button onclick="deleteData(${i})">Delete</button> <button onclick="update(${i})">Edit</button></td>
+        </tr>
+        `;
+  }
+  document.getElementById("tbody").innerHTML = table;
+}
+readData();
+
+function deleteData(i) {
+  dataArry.splice(i, 1);
+  localStorage.results = JSON.stringify(dataArry);
+  readData();
+}
+
+function update(i) {
+  submit.innerHTML = "Update";
+  mode = "edit";
+  Name.value = dataArry[i].name;
+  birthdate.value = dataArry[i].birthdate;
+  address.value = dataArry[i].address;
+  arryIndex = i;
+  datePlaceholder();
+  scroll({
+    top: 0,
+    behavior: "smooth",
+  });
+};
+
+function datePlaceholder() {
+  if (birthdate.value == "") {
+    birthdate.style.color = "rgba(0,0,0,0.5)";
+  } else if (birthdate.value != "") {
+    birthdate.style.color = "rgb(0,0,0)";
+  }
+}
+
+birthdate.onblur = datePlaceholder;
+birthdate.onfocus = function () {
+  birthdate.style.color = "rgb(0,0,0) ";
+};
+
+let searchMode = "name";
+function getSearchMode(id) {
+  if (id == "searchNameBtnTeachers") {
+    searchMode = "name";
+  } else if (id == "searchNumBtnTeachers") {
+    searchMode = "number";
+  }
+
+  searchInput.placeholder = "Search by " + searchMode;
+
+  searchInput.focus();
+}
+
+function search(value) {
+  let table = "";
+
+  for (let i = 0; i < dataArry.length; i++) {
+    if (searchMode == "name") {
+      if (dataArry[i].name.includes(value)) {
+        table += `
+        <tr>
+                <td class="td-Num">${dataArry[i].Num}</td>
+                <td class="td-border"></td>
+                <td class="td-Name">${dataArry[i].name}</td>
+                <td class="td-border"></td>
+                <td class="td-Birthdate">${dataArry[i].birthdate}</td>
+                <td class="td-border"></td>
+                <td class="td-Address">${dataArry[i].address}</td>
+                <td class="td-border"></td>
+                <td class="td-Delete-and-Edit"><button onclick="deleteData(${i})">Delete</button> <button onclick="update(${i})">Edit</button></td>
+        </tr>
+        `
+      }
+    } else if (searchMode == "number") {
+      if (dataArry[i].Num.toString().includes(value)) {
+        table += `
+        <tr>
+                <td class="td-Num">${dataArry[i].Num}</td>
+                <td class="td-border"></td>
+                <td class="td-Name">${dataArry[i].name}</td>
+                <td class="td-border"></td>
+                <td class="td-Birthdate">${dataArry[i].birthdate}</td>
+                <td class="td-border"></td>
+                <td class="td-Address">${dataArry[i].address}</td>
+                <td class="td-border"></td>
+                <td class="td-Delete-and-Edit"><button onclick="deleteData(${i})">Delete</button> <button onclick="update(${i})">Edit</button></td>
+        </tr>
+        `;
+      }
+    }
+  }
+  document.getElementById("tbody").innerHTML = table;
+  
+}
