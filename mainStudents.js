2e88a4bd382579120ee3b365c754c6b985fd032c
zed albash <zedalbash342@gmail.com>
2024-06-20 16:20:00 +0300

    Students pfp done

--- mainStudents.js
+++ mainStudents.js
@@ -2,7 +2,11 @@ var Name = document.getElementById("Name");
 var birthdate = document.getElementById("Birthdate");
 var Class = document.getElementById("class");
 var address = document.getElementById("Address");
-var submit = document.getElementById("submit")
+var submit = document.getElementById("submit");
+var searchInput = document.getElementById("searchInput");
+
+var imgUrl;
+
 var mode = "create"
 
 var dataArry = [];
@@ -37,6 +41,7 @@ if (Name.value != "", birthdate != "", Class != "", address != ""){
         Birthdate: birthdate.value,
         oClass: Class.value,
         Address: address.value,
+        Img: imgUrl
     };
     if (mode == "create"){
         result.Num += 1;
@@ -63,6 +68,10 @@ function clearInput()
     birthdate.value = "";
     Class.value = "";
     Address.value = "";
+    showImg.src = "icons/plus-icon.png";
+    showImg.style.width = "20%";
+    showImg.style.height = "20%";
+    imgText.style.display = "block";
 }
 
 function datePlaceholder(){
@@ -83,6 +92,8 @@ function readData(){
     for (let i = 0; i < dataArry.length; i++){
         table += `
         <tr>
+                <td class="td-Pfp"><img src="${dataArry[i].Img}" alt="" onmouseenter="imgPreview(this.src)" onmouseleave="stopImgPreview()"></td>
+                <td class="td-border"></td>
                 <td class="td-Num">${dataArry[i].Num}</td>
                 <td class="td-border"></td>
                 <td class="td-Name">${dataArry[i].name}</td>
@@ -114,6 +125,11 @@ function update(i){
     birthdate.value = dataArry[i].Birthdate;
     Class.value = dataArry[i].oClass;
     address.value = dataArry[i].Address;
+    showImg.src = dataArry[i].Img;
+    imgUrl = dataArry[i].Img;
+    showImg.style.width = "100%"
+    showImg.style.height = "100%"
+    imgText.style.display = "none"
     arryIndex = i;
     datePlaceholder();
     scroll({
@@ -123,13 +139,23 @@ function update(i){
 };
 
 var searchMode = "name";
+function getSearchMode(id){
+    if (id == "searchNameBtn"){
+        searchMode = "name";
+    }
+    else if(id == "searchNumBtn"){
+        searchMode = "number";
+    }
 
+    searchInput.placeholder = "Search by " + searchMode;
+}
 function search(value){
     let table = "";
     for (let i = 0; i < dataArry.length; i++){
         if (searchMode == "name"){
             if(dataArry[i].name.includes(value)){
                 table += `<tr>
+                <td class="td-Pfp"><img src="${dataArry[i].Img}" alt="" onmouseenter="${imgPreview(dataArry[i].Img)}" ></td>
                 <td class="td-Num">${dataArry[i].Num}</td>
                 <td class="td-border"></td>
                 <td class="td-Name">${dataArry[i].name}</td>
@@ -148,6 +174,8 @@ function search(value){
             if(dataArry[i].Num.toString().includes(value)){
                 table +=
                 `<tr>
+                <td class="td-Pfp"><img src="${dataArry[i].Img}" alt=""></td>
+                <td class="td-border></td>
                 <td class="td-Num">${dataArry[i].Num}</td>
                 <td class="td-border"></td>
                 <td class="td-Name">${dataArry[i].name}</td>
@@ -165,4 +193,43 @@ function search(value){
     
     }
     document.getElementById("tbody").innerHTML = table;
+}
+
+var imgPath = document.getElementById("inputFileImg");
+var showImg = document.getElementById("showImg");
+var imgText = document.getElementById("imgText");
+const reader = new FileReader();   
+
+imgPath.onchange = function () {
+     
+
+reader.addEventListener("load", function (){
+ imgUrl = reader.result;
+ showImg.src = reader.result;
+ showImg.style.width = "100%"
+ showImg.style.height = "100%"
+ imgText.style.display = "none"
+},false)
+
+if (imgPath){
+    reader.readAsDataURL(imgPath.files[0]);
+} 
+
+}
+
+function imgPreview(i){
+
+     
+    showImg.src = i;
+    showImg.style.width = "100%"
+    showImg.style.height = "100%"
+    imgText.style.display = "none"
+}
+
+function stopImgPreview(){
+    
+    showImg.src = "icons/plus-icon.png";
+    showImg.style.width = "20%";
+    showImg.style.height = "20%";
+    imgText.style.display = "block";
 }
